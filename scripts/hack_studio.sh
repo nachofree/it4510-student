a59c5c682c5012be1ce27bd482ba9cd0ba9b362e
Joe <joe@cs.dixie.edu>
2023-02-01 12:42:29 -0700

    update to add windows clone image

--- scripts/hack_studio.sh
+++ scripts/hack_studio.sh
@@ -12,7 +12,7 @@ prefix=s23-it4510
 declare -A toclone
 #this is a list of images that I want to clone when I run the command and how much ram each should be given
 
-toclone=( ["s23-hs-ubuntu-gui"]="1024" ["s23-hs-pfsense"]="512" ["s23-hs-kali"]="2048" ["hs-metasploit"]="512" )
+toclone=( ["s23-hs-ubuntu-gui"]="1024" ["s23-hs-pfsense"]="512" ["s23-hs-kali"]="2048" ["s23-hs-metasploit"]="512" ["s23-hs-win7"]="2048" )
 
 
 function usage(){
